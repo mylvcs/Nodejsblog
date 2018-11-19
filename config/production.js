fb6330a0205ecfad811f474fc3709d83039a4c13
mylvcs <wmy0828@nuaa.edu.cn>
2018-11-19 12:03:36 -0800

    push

--- config/production.js
+++ config/production.js
@@ -1,9 +1,4 @@
 module.exports = {
-    port: 3000,
-    session: {
-      secret: 'myblog',
-      key: 'myblog',
-      maxAge: 2592000000
-    },
+    port: 80,
     mongodb : 'mongodb://blogadmin:blogadmin123@ds163683.mlab.com:63683/myblog'
 }
